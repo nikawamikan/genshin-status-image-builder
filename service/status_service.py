cb80ce586a52dcfd57f6c2161a8a9e146426ce46
nikawamikan <wkwktktkkrkr@outlook.jp>
2023-06-01 22:00:06 +0000

    ステータスデータ生成用のserviceの修正

--- service/status_service.py
+++ service/status_service.py
@@ -1,9 +1,19 @@
 from decimal import Decimal
-import aiohttp
-from decimal import Decimal
 import re
 import datetime
-from model.status_model import Artifact, ArtifactStatus, Character, UserData, Weapon, Skill
+import model.status_model as status_model
+import model.enka_model as enka_model
+from repository.util_data import CHARACTER_DATA_DICT
+
+
+PERCENT_PATTERN = re.compile(
+    r"PERCENT|CRITICAL|FIGHT_PROP_CHARGE_EFFICIENCY|_ADD_HURT")
+
+
+def get_suffix(name: str):
+    if PERCENT_PATTERN.search(name):
+        return "%"
+    return ""
 
 
 def none_to_empty_char(data: dict[str, str], keys: list[str]) -> str:
@@ -16,27 +26,8 @@ def none_to_empty_char(data: dict[str, str], keys: list[str]) -> str:
         return ""
 
 
-ELEMENT = {
-    "Wind": "風",
-    "Rock": "岩",
-    "Electric": "雷",
-    "Grass": "草",
-    "Water": "水",
-    "Fire": "炎",
-    "Ice": "氷"
-}
-PERCENT_PATTERN = re.compile(r"%|会心|チャ|元素ダメ")
-ELEMENT_DAMAGE_TYPES = {
-    "30": "物理ダメージ",
-    "40": "炎元素ダメージ",
-    "41": "雷元素ダメージ",
-    "42": "水元素ダメージ",
-    "43": "草元素ダメージ",
-    "44": "風元素ダメージ",
-    "45": "岩元素ダメージ",
-    "46": "氷元素ダメージ"
-}
 ELEMENT_MAP = {
+    "": "30",
     "Fire": "40",
     "Electric": "41",
     "Water": "42",
@@ -46,110 +37,95 @@ ELEMENT_MAP = {
     "Ice": "46",
 }
 
+ELEMENT_DAMAGE_TYPES = {
+    v: k for k, v in ELEMENT_MAP.items()
+}
+
 
 def check_traveler(id: int):
     return id in ["10000007", "10000005"]
 
 
-headers = {
-    'User-Agent': 'Miao-Plugin/3.0 (Lunux; Intel Ubuntu 22.04)'}
-
-
-async def get_json(uid: int) -> dict:
-    url = f"https://enka.network/api/uid/{uid}"
-    async with aiohttp.ClientSession(raise_for_status=True, headers=headers) as session:
-        async with session.get(url) as response:
-            resp = await response.json()
-    return resp
-
-
-def get_characters(uid: int, create_date: str, json: dict) -> list[Character]:
-    if 'avatarInfoList' in json:
-        characters = [
-            get_character_status(uid, create_date, v) for v in json['avatarInfoList']
-        ]
-    else:
-        characters = []
-
+def get_characters(
+    uid: int,
+    create_date: str,
+    avatar_info_list: list[enka_model.AvatarInfo]
+) -> list[status_model.Character]:
+    characters = [
+        get_character_status(uid, create_date, v) for v in avatar_info_list
+    ]
     return characters
 
 
-def get_user_data(json: dict) -> UserData:
-    uid = json['uid']
+def get_user_data(enka: enka_model.Enka) -> status_model.UserData:
     create_date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    char_list = get_characters(uid, create_date, json)
-    char_name_map = {c.name: i for i, c in enumerate(char_list)}
-    return UserData(
-        uid=json['uid'],
-        level=json['playerInfo']['level'],
-        signature=none_to_empty_char(json, ['playerInfo', 'signature']),
-        world_level=json['playerInfo']['worldLevel'],
-        name_card_id=json['playerInfo']['nameCardId'],
-        finish_achievement_num=json['playerInfo']['finishAchievementNum'],
-        tower_floor_index=json['playerInfo']['towerFloorIndex'],
-        tower_level_index=json['playerInfo']['towerLevelIndex'],
-        nickname=json['playerInfo']['nickname'],
+    char_name_map = {CHARACTER_DATA_DICT[c.avatarId].name: i for i, c in enumerate(
+        enka.playerInfo.showAvatarInfoList)}
+    char_list = get_characters(enka.uid, create_date, enka.avatarInfoList)
+    return status_model.UserData(
+        uid=enka.uid,
+        level=enka.playerInfo.level,
+        signature=enka.playerInfo.signature,
+        world_level=enka.playerInfo.worldLevel,
+        name_card_id=enka.playerInfo.nameCardId,
+        finish_achievement_num=enka.playerInfo.finishAchievementNum,
+        tower_floor_index=enka.playerInfo.towerFloorIndex,
+        tower_level_index=enka.playerInfo.towerLevelIndex,
+        nickname=enka.playerInfo.nickname,
         create_date=create_date,
         char_name_map=char_name_map,
         characters=char_list,
     )
 
 
-def get_artifact(json: dict):
-    flat = json["flat"]
-    icon_name = flat["icon"]
-    icon = f'https://enka.network/ui/{flat["icon"]}.png'
-    main_name = NAME_HASH[flat["reliquaryMainstat"]["mainPropId"]]
-    main_value = str(flat["reliquaryMainstat"]["statValue"])
+def get_artifact(equip: enka_model.Equip):
+    flat = equip.flat
+    icon_name = flat.icon
+    main_name = flat.reliquaryMainstat.mainPropId
+    main_value = flat.reliquaryMainstat.statValue
+    flat.reliquarySubstats
     suffix = get_suffix(main_name)
-    level = json["reliquary"]["level"] - 1
-    artifact_set_name = NAME_HASH[flat["setNameTextMapHash"]]
-    star = flat["rankLevel"]
+    level = equip.reliquary.level - 1
+    star = flat.rankLevel
     status = [
-        ArtifactStatus(
-            value=round(Decimal(v["statValue"]), 1),
-            name=NAME_HASH[v["appendPropId"]],
-            suffix=get_suffix(NAME_HASH[v["appendPropId"]]),
-        ) for v in flat["reliquarySubstats"]
+        status_model.ArtifactStatus(
+            value=round(v.statValue, 1),
+            name=v.appendPropId,
+            suffix=get_suffix(v.appendPropId),
+        ) for v in flat.reliquarySubstats
     ]
-    return Artifact(
+    return status_model.Artifact(
         icon_name=icon_name,
-        icon=icon,
         main_name=main_name,
         star=star,
         main_value=main_value,
         suffix=suffix,
         level=level,
-        artifact_set_name=artifact_set_name,
         status=status,
     )
 
 
-def get_artifacts(json: list[dict]) -> dict[str, Artifact]:
+def get_artifacts(equip_list: list[enka_model.Equip]) -> dict[str, status_model.Artifact]:
     return {
-        v["flat"]["equipType"]: get_artifact(v)
-        for v in json
+        equip.flat.equipType: get_artifact(equip)
+        for equip in equip_list
     }
 
 
-def get_weapon(json: dict):
-    flat = json["flat"]
-    name = NAME_HASH[flat["nameTextMapHash"]]
-    icon_name = flat["icon"]
-    icon = f'https://enka.network/ui/{icon_name}.png'
-    main_name = NAME_HASH[flat["weaponStats"][0]["appendPropId"]]
-    main_value = flat["weaponStats"][0]["statValue"]
-    sub_name = ""
-    sub_value = ""
-    if len(flat["weaponStats"]) == 2:
-        sub_name = NAME_HASH[flat["weaponStats"][1]["appendPropId"]]
-        sub_value = flat["weaponStats"][0]["statValue"]
-    level = json["weapon"]["level"]
-    rank = flat["rankLevel"]
-    return Weapon(
-        name=name,
+def get_weapon(equip: enka_model.Equip):
+    flat = equip.flat
+    icon_name = flat.icon
+    main_name = flat.weaponStats[0].appendPropId
+    main_value = flat.weaponStats[0].statValue
+    sub_name = None
+    sub_value = None
+    if len(flat.weaponStats) == 2:
+        sub_name = flat.weaponStats[1].appendPropId
+        sub_value = flat.weaponStats[1].statValue
+    level = equip.weapon.level
+    rank = flat.rankLevel
+    return status_model.Weapon(
         icon_name=icon_name,
-        icon=icon,
         main_name=main_name,
         main_value=main_value,
         sub_name=sub_name,
@@ -161,12 +137,10 @@ def get_weapon(json: dict):
 
 def get_skills(id: str, skill_levels: dict[str, int], extra_skill_levels: dict[str, int]):
     return [
-        Skill(
-            name=v.name,
-            icon=v.icon,
+        status_model.Skill(
             level=skill_levels[v.id],
             add_level=extra_skill_levels[v.proud_id] if v.proud_id in extra_skill_levels else 0,
-        )for v in CHARACTER[id].skills
+        )for v in CHARACTER_DATA_DICT[id].skills
     ]
 
 
@@ -182,20 +156,20 @@ def get_constellations(chara: dict):
     return constellations
 
 
-def get_elemental_name_value(id: str, json: dict):
+def get_elemental_name_value(id: str, avatar_info: enka_model.AvatarInfo):
 
     elemental_name = None
     elemental_value = None
 
-    max_value = 0
+    max_value = Decimal(0)
     max_key = None
     for k in ELEMENT_DAMAGE_TYPES.keys():
-        if max_value < json["fightPropMap"][k]:
-            max_value = json["fightPropMap"][k]
+        if max_value < avatar_info.fightPropMap[k]:
+            max_value = avatar_info.fightPropMap[k]
             max_key = k
     if max_key is not None:
-        if max_value == json["fightPropMap"][ELEMENT_MAP[CHARACTER[id].element]]:
-            elemental_name = ELEMENT_DAMAGE_TYPES[ELEMENT_MAP[CHARACTER[id].element]]
+        if max_value == avatar_info.fightPropMap[ELEMENT_MAP[CHARACTER_DATA_DICT[id].element]]:
+            elemental_name = CHARACTER_DATA_DICT[id].element
         else:
             elemental_name = ELEMENT_DAMAGE_TYPES[max_key]
         elemental_value = f"{round(max_value*100, 1)}%"
@@ -203,47 +177,36 @@ def get_elemental_name_value(id: str, json: dict):
     return (elemental_name, elemental_value)
 
 
-def get_character_status(uid: int, create_date: str, json: dict):
-    id = str(json["avatarId"])
+def get_character_status(uid: int, create_date: str, avatar_info: enka_model.AvatarInfo):
+    id = avatar_info.avatarId
     if check_traveler(id):
-        id = f"{id}-{json['skillDepotId']}"
-    name = CHARACTER[id].name
-    english_name = CHARACTER[id].english_name
-    image = CHARACTER[id].gacha_icon_url
-    element = CHARACTER[id].element
-    star = CHARACTER[id].quality
-    constellations = get_constellations(json)
+        id = f"{id}-{avatar_info.skillDepotId}"
+    star = CHARACTER_DATA_DICT[id].quality
+    constellations = len(CHARACTER_DATA_DICT[id].consts)
+    level = int(avatar_info.propMap["4001"].val)
+    base_hp = int(avatar_info.fightPropMap["1"])
+    added_hp = avatar_info.fightPropMap["2000"] - base_hp
 
-    level = str(json["propMap"]["4001"]["val"])
-    base_hp = str(round(json["fightPropMap"]["1"]))
-    added_hp = str(
-        round(json["fightPropMap"]["2000"]) - round(json["fightPropMap"]["1"]))
-    base_attack = str(round(json["fightPropMap"]["4"]))
-    added_attack = str(
-        round(json["fightPropMap"]["2001"]) - round(json["fightPropMap"]["4"]))
-    base_defense = str(round(json["fightPropMap"]["7"]))
-    added_defense = str(
-        round(json["fightPropMap"]["2002"]) - round(json["fightPropMap"]["7"]))
-    critical_rate = str(round(json["fightPropMap"]["20"] * 100, 1))
-    critical_damage = str(round(json["fightPropMap"]["22"] * 100, 1))
-    charge_efficiency = str(round(json["fightPropMap"]["23"] * 100, 1))
-    elemental_mastery = str(round(json["fightPropMap"]["28"]))
-    love = int(round(json["fetterInfo"]["expLevel"]))
+    base_attack = int(avatar_info.fightPropMap["4"])
+    added_attack = int(avatar_info.fightPropMap["2001"]) - base_attack
+    base_defense = int(avatar_info.fightPropMap["7"])
+    added_defense = int(avatar_info.fightPropMap["2002"]) - base_defense
+    critical_rate = round(avatar_info.fightPropMap["20"] * 100, 1)
+    critical_damage = round(avatar_info.fightPropMap["22"] * 100, 1)
+    charge_efficiency = round(avatar_info.fightPropMap["23"] * 100, 1)
+    elemental_mastery = int(avatar_info.fightPropMap["23"])
+    love = avatar_info.fetterInfo.expLevel
 
-    elemental_name, elemental_value = get_elemental_name_value(id, json)
+    elemental_name, elemental_value = get_elemental_name_value(id, avatar_info)
     skills = get_skills(
         id,
-        json["skillLevelMap"],
-        json["proudSkillExtraLevelMap"] if "proudSkillExtraLevelMap" in json else {}
+        avatar_info.skillLevelMap,
+        avatar_info.proudSkillExtraLevelMap
     )
-    artifacts = get_artifacts(json["equipList"][:-1])
-    weapon = get_weapon(json["equipList"][-1])
-    return Character(
+    artifacts = get_artifacts(avatar_info.equipList[:-1])
+    weapon = get_weapon(avatar_info.equipList[-1])
+    return status_model.Character(
         id=id,
-        name=name,
-        english_name=english_name,
-        image=image,
-        element=element,
         star=star,
         constellations=constellations,
         level=level,
@@ -264,5 +227,6 @@ def get_character_status(uid: int, create_date: str, json: dict):
         artifacts=artifacts,
         weapon=weapon,
         uid=uid,
-        create_date=create_date
+        create_date=create_date,
+        costume=avatar_info.costumeId
     )
