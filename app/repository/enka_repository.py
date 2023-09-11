7b8bac2d5db2b6a66fd27fe645bdd38b195c5544
nikawamikan <wkwkwtktkkrkr@outlook.jp>
2023-09-11 11:37:40 +0000

    fix: EnkaAPIのエラーハンドリング修正
    enka側で出力されるエラーをラッピングして独自エラーとして定義
    エラーの内容をメッセージとして格納し返却するように変更

--- app/repository/enka_repository.py
+++ app/repository/enka_repository.py
@@ -1,10 +1,29 @@
 import lib.downloader as downloader
+from aiohttp import client_exceptions
+from fastapi import HTTPException
 from model.enka_model import Enka
 
-async def get_enka_model(uid: int):
-    data = await downloader.json_download(
-        url=f"https://enka.network/api/uid/{uid}"
-    )
-    enka_model = Enka(** data)
 
-    return enka_model
+MESSAGES = {
+    400: "UIDのフォーマットが間違っています。\n半角数字で入力してください",
+    404: "入力されたものが存在するUIDではありません\nもう一度確認してやり直してください。",
+    424: "ゲームメンテナンスやアップデートの影響により\nEnka.network（ビルドデータを取得するサービス）が停止している状態です。\nこのエラーはしばらく時間をおいてから試すと回復している可能性が高いです。",
+    429: "処理が追いついていません。\nしばらくしても解決しない場合は、開発者に対してコンタクトをとってください。",
+    500: "Enka.network（ビルドデータを取得するサービス）のサーバーにエラーが発生しています。\n詳しくはEnkaのTwitterを確認してください。https://twitter.com/EnkaNetwork",
+    503: "Enka.network（ビルドデータを取得するサービス）サーバーの一時停止中です。\nしばらくお待ちください。※開発者はこれについて確認ぐらいしか取れないです。\n詳しくはEnkaのTwitterを確認してください"
+}
+
+
+async def get_enka_model(uid: int):
+    try:
+        data = await downloader.json_download(
+            url=f"https://enka.network/api/uid/{uid}"
+        )
+        enka_model = Enka(** data)
+
+        return enka_model
+
+    except client_exceptions.ClientResponseError as e:
+        print(e)
+        raise HTTPException(
+            status_code=451, detail=f"{MESSAGES[e.status]} \ncode: {e.status}")
